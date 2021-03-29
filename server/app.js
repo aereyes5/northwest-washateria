275f8300707114676f48d234493f105e3c105897
Alyssa Reyes <alyssareyes1010@gmail.com>
2021-03-28 23:12:57 -0500

    Connection edit

--- server/app.js
+++ server/app.js
@@ -1,3 +1,37 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+app.use(express.json());
+
+//Create connection
+const db = mysql.createConnection({
+    host     : 'us-cdbr-east-03.cleardb.com',
+    user     : 'bbf182819b2860',
+    password : 'adf022e7',
+    database: 'heroku_511310a7e136d98'
+})
+
+//Connect
+db.connect(function(err) {
+    if(err){
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + db.threadId);
+})
+
+db.query('SELECT * FROM customers', function(error, results, fields) {
+    if(error) throw error;
+    console.log(results);
+});
+
+db.end();
+
+
+
+
+
 // const express = require('express'),
 //       bodyParser = require('body-parser'),
 //       logger = require('morgan'),
@@ -23,36 +57,21 @@
 // })
 
 
-const express = require('express');
-const mysql = require('mysql');
+// let port = process.env.PORT || 3306;
+// app.listen(port, () => {
+//     console.log(`Server started on port ${port}...`);
+// })
 
-const app = express();
-
-app.get('/', (req, res) => {
-    res.render('')
-})
-
-//Create connection
-const db = mysql.createConnection({
-    host     : 'us-cdbr-east-03.cleardb.com',
-    user     : 'bbf182819b2860',
-    password : 'adf022e7',
-    database: 'heroku_511310a7e136d98'
-})
-
-//Connect
-db.connect((err) => {
-    if(err){
-        throw err;
-    }
-    console.log('MySQL Connected...')
-})
-
-
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log('Server started on port ' + port);
-})
+// app.get('/customers', (req, res) => {
+//     db.query('SELECT * FROM customers', (err, rows, fields) => {
+//         if(!err){
+//             res.send(rows);
+//         }
+//         else{
+//             console.log(err);
+//         }
+//     })
+// })
 
 
 
