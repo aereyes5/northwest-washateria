b16438475d25f36e8cf90a1e683fa836a985b289
Alyssa Reyes <alyssareyes1010@gmail.com>
2021-03-28 22:00:57 -0500

    Connected DB

--- server/app.js
+++ server/app.js
@@ -1,23 +1,59 @@
-const express = require('express'),
-      bodyParser = require('body-parser'),
-      logger = require('morgan'),
-      cors = require('cors'),
-      port = process.env.PORT || 3000,
-      posts = require('./routes/api/post'),
-      purchases = require('./routes/api/purchase')
-      app = express();
+// const express = require('express'),
+//       bodyParser = require('body-parser'),
+//       logger = require('morgan'),
+//       cors = require('cors'),
+//       port = process.env.PORT || 3000,
+//       posts = require('./routes/api/post'),
+//       purchases = require('./routes/api/purchase')
+//       app = express();
 
-require('./mongooseConnect');
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(cors());
-app.use('/', posts);
-app.use('/purchases', purchases);
+// require('./mongooseConnect');
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(cors());
+// app.use('/', posts);
+// app.use('/purchases', purchases);
 
-app.listen(port, function(error) {
-    if(error) {
+// app.listen(port, function(error) {
+//     if(error) {
+//         throw err;
+//     }
+//     console.log(`Web Application is running on port ${port}`)
+//     console.log(`http://localhost:${port}`)
+// })
+
+
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.render('')
+})
+
+//Create connection
+const db = mysql.createConnection({
+    host     : 'us-cdbr-east-03.cleardb.com',
+    user     : 'bbf182819b2860',
+    password : 'adf022e7',
+    database: 'heroku_511310a7e136d98'
+})
+
+//Connect
+db.connect((err) => {
+    if(err){
         throw err;
     }
-    console.log(`Web Application is running on port ${port}`)
-    console.log(`http://localhost:${port}`)
+    console.log('MySQL Connected...')
 })
+
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+})
+
+
+
+
--- server/mongooseConnect.js
+++ server/mongooseConnect.js
@@ -12,3 +12,35 @@ mongoose.connect(connectionString, {
  
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'Failed to Connect'))
+
+
+
+
+
+// const mysql = require('mysql');
+
+// // First you need to create a connection to the database
+// // Be sure to replace 'user' and 'password' with the correct values
+// const con = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'jandroo_hdo',
+//   password: 'NWwashateria',
+//   database: 'washasteria'
+// });
+
+// con.connect((err) => {
+//   if(err){
+//     console.log('Error connecting to Db');
+//     return;
+//   }
+//   console.log('Connection established');
+// });
+
+// con.end((err) => {
+//   // The connection is terminated gracefully
+//   // Ensures all remaining queries are executed
+//   // Then sends a quit packet to the MySQL server.
+// });
+
+
+
