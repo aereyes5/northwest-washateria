8e1a434d6f7b2d20f2d8d56bb5603d0f6f18295e
jandrooo <alejandroventura74@gmail.com>
2021-04-04 18:41:20 -0500

    Methods finished

--- client/src/components/Products.vue
+++ client/src/components/Products.vue
@@ -20,29 +20,55 @@ export default {
    },
    methods: {
        getCustomers(){
-
+           services.getProducts().then(response => {
+               this.products = response
+               console.log(this.products)
+           }).catch(e => {
+               console.log(e);
+           })
        },
-       deleteProdut(){
-
+       deleteProduct(){
+           if(!Array.isArray(this.selected) || !this.selected.length){
+               this.status2 = "Please select a record to delete";
+           }
+           else{
+               this.status2 = "";
+               services.deleteProduct(this.selected[0].productID)
+               window.location.reload()
+           }
        },
        onRowSelected(items){
            this.selected = items
            console.log(items)
        },
        getProductById(){
-           if(this.productId == null){
+           if(this.productID == null){
                this.status = "Please enter product name"
            }
            else{
                this.status = "";
                try{
-                   //function to find product by name
+                   services.getProductByProductID(this.productID).then(response => {
+                       this.products = response
+                       console.log(this.products)
+                   })
                }catch(err){
                    console.log(err)
                }
            }
        },
-       
+       setProductID(){
+           if(!Array.isArray(this.selected) || !this.selected.length){
+               this.status2 = "Please select a record to update"
+           }
+           else{
+               this.updateProduct(this.selected[0].productID)
+           }
+       },
+       updateProduct(productID){
+           this.$store.commit('productID', {productID})
+           this.$router.push({name: 'UpdateProduct'})
+       }
    },
    created(){
        this.getProducts()
