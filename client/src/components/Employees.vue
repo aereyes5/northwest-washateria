6cf86956ff328a3949825d464fcd05a25381886f
jandrooo <alejandroventura74@gmail.com>
2021-04-05 20:02:35 -0500

    Finished up employees vue file, testing coming soon

--- client/src/components/Employees.vue
+++ client/src/components/Employees.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <b-input-group class="mt-3">
-            <b-form-input placeholder="Enter Product's ID number" v-model="employeeID"></b-form-input>
+            <b-form-input placeholder="Enter Employee's ID number" v-model="employeeID"></b-form-input>
             <b-input-group-append>
             <b-button variant="primary" v-on:click="getEmployeeByID">Search</b-button>
             </b-input-group-append>
@@ -52,9 +52,68 @@ export default {
     },
     methods: {
         getEmployees(){
-            
+            services.getEmployees().then(response => {
+                this.employees = response
+                this.employeeID = null
+                console.log(this.employees)
+            }).catch(e => {
+                console.log(e);
+            })
+        },
+
+        deleteProduct(){
+            if(!Array.isArray(this.selected) || !this.selected.length){
+                this.status2 = "Please select a record to delete";
+            }
+            else{
+                this.status2 = "";
+                services.deleteEmployee(this.selected[0].employeeID)
+                window.location.reload()
+            }
+        },
+
+        onRowSelected(items){
+            this.selected = items
+            console.log(items)
+        },
+
+        getEmployeeByID(){
+            if(this.employeeID == null){
+                this.status = "Please enter employee ID"
+            }
+            else{
+                this.status = "";
+                try{
+                    services.getEmployeeByID(this.employeeID).then(response => {
+                        this.employees = response
+                        console.log(this.employees)
+                    })
+                }catch(err){
+                    console.log(err)
+                }
+            }
+        },
+
+        setEmployeeID(){
+            if(!Array.isArray(this.selected) || !this.selected.length){
+                this.status2 = "Please select a record to update"
+            }
+            else{
+                this.updateEmployee(this.selected[0].employeeID)
+            }
+        },
+
+        updateEmployee(employeeID){
+            this.$store.commit('eEmployeeID', {employeeID})
+            this.$router.push({name: 'UpdateEmployee'})
         }
+    },
+    created(){
+        this.getEmployees()
     }
-    
 }
 </script>
+
+<style scoped>
+
+</style>
