b6b1c88d690fd151abccf72b45a3c7acca4c7480
Alyssa Reyes <alyssareyes1010@gmail.com>
2021-04-13 10:19:30 -0500

    Report Formatting DONE

--- client/src/components/orderReport.vue
+++ client/src/components/orderReport.vue
@@ -1,28 +1,37 @@
 <template>
     <div>
         <img src="@/assets/genreport.png" width=750px heigth=150px alt="Genereate Reports">
-        <b-form-input placeholder="Search..." v-model="filter" type="search"></b-form-input>
-        <b-button class="darkmode-ignore" variant="success">
-            <downloadExcel :fields="fieldsForExcel" :data="selected" name="Report" worksheet="Orders" type="xls">
-                Excel Report
-            </downloadExcel>
-        </b-button>
-        <b-button class="darkmode-ignore" variant="primary" v-on:click="createNewReport">PDF Report</b-button>
-        <b-button class="darkmode-ignore" variant="info">
-            <downloadExcel :fields="fieldsForExcel" :data="selected" name="Report" type="csv">
-                CSV Report
-            </downloadExcel>
-        </b-button>
-        <br>
-        <b-button class="darkmode-ignore" variant="outline-success" v-on:click="selectAllRows">Select All Rows
-        </b-button>
-        <b-button class="darkmode-ignore" variant="outline-danger" v-on:click="clearSelected">Clear Selected Rows
-        </b-button>
-        <b-button class="darkmode-ignore" v-bind:to="'Orders'" variant="danger">Cancel</b-button>
+        <b-container fluid="md">
+        
+            <b-form-input placeholder="Search..." v-model="filter" type="search"></b-form-input>
+            <b-row align-h="center">
+                <b-button-group>
+                        <b-button class="darkmode-ignore" variant="outline-success" v-on:click="selectAllRows">Select All Rows
+                        </b-button>
+                        <b-button class="darkmode-ignore" variant="outline-danger" v-on:click="clearSelected">Clear Selected Rows
+                        </b-button>
+                        <b-dropdown right text="Download Report" variant="secondary">
+                            <b-dropdown-item class="darkmode-ignore">
+                                <downloadExcel :fields="fieldsForExcel" :data="selected" name="Report" worksheet="Orders" type="xls">
+                                    Excel Report
+                                </downloadExcel>
+                            </b-dropdown-item>
+                            <b-dropdown-item class="darkmode-ignore" v-on:click="createNewReport">PDF Report</b-dropdown-item>
+                            <b-dropdown-item class="darkmode-ignore">
+                                <downloadExcel :fields="fieldsForExcel" :data="selected" name="Report" type="csv">
+                                    CSV Report
+                                </downloadExcel>
+                            </b-dropdown-item>
+                        </b-dropdown> 
+                        <b-button class="darkmode-ignore" v-bind:to="'Orders'" variant="danger">Cancel</b-button> 
+                </b-button-group>
+            </b-row>
+            <b-table id="report" :items="orders" :fields="fields" :filter="filter" :select-mode="selectMode" striped
+                responsive="sm" ref="selectableTable" selectable hover @row-selected="onRowSelected">
+            </b-table>
+        </b-container>
 
-        <b-table id="report" :items="orders" :fields="fields" :filter="filter" :select-mode="selectMode" striped
-            responsive="sm" ref="selectableTable" selectable hover @row-selected="onRowSelected">
-        </b-table>
+
     </div>
 </template>
 
